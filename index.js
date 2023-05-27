29042c140b23fb01d289dd155ddf613b98e1ebd8
Alexis <al628814@ohio.edu>
2023-05-26 21:58:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards() {
+    const names = ["Guadalupe", "Ollie", "Aki"];
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        cards[i] = message;
+    }
+
+    return cards;
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+ 
